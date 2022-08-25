f3fbc6417e75a889b9531f4529f5b1a9e7548d72
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-08-25 18:41:07 +0100

    added gh pages

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
     return (
         <section id="about" className="bg-gray-800" style={{backgroundColor: '#3E63F5'}}>
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:flex-grow md:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mt-16  md:mb-0 items-center text-center">
+                <div className="md:w-1/2 lg:flex-grow md:pr-24  flex flex-col md:items-start md:text-left mt-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Harry Parker
                     </h1>
